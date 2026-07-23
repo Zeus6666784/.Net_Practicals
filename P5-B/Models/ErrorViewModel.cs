b282384f7cb691a22161b41065ba3331a79be3d6
LAVKUSH VISHWAKARMA <lavkvishmvlu@gmail.com>
2026-07-23 22:47:49 +0530

    hi.

---
+++ P5-B/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P5_B.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
